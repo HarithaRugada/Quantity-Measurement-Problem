6f3a6f11f500a7dce86b16e33d4e23e3a55002ef
HarithaRugada <haritharugada@gmail.com>
2020-07-14 20:05:39 +0530

    REFACTORED : Code for handling conversions related to centimeters

--- src/main/java/com/quantitymeasurement/utility/UnitConversion.java
+++ src/main/java/com/quantitymeasurement/utility/UnitConversion.java
@@ -7,7 +7,10 @@ public enum UnitConversion {
     INCH_TO_YARD(1 / 36.0),
     YARD_TO_INCH(36.0),
     YARD_TO_FEET(3.0),
-    INCH_TO_CENTIMETER(2.54);
+    INCH_TO_CENTIMETER(2.54),
+    CENTIMETER_TO_INCH(1 / 2.54),
+    FEET_TO_CENTIMETER(30.48),
+    CENTIMETER_TO_FEET(0.03);
 
     public double unit;
 
